fecdf76e22e1710aefd9e3a2c2065d402223c640
cice3337 <48628994+cice3337@users.noreply.github.com>
2019-07-23 18:45:47 -0400

    Add files via upload

---
+++ MapNetworkDriveScheduledTask.ps1
@@ -0,0 +1,5 @@
+﻿$ST_A = New-ScheduledTaskAction –Execute C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Argument '-NoLogo -NoProfile -NonInteractive -WindowStyle Hidden -ExecutionPolicy ByPass -command New-PSDrive -Name "Z" -PSProvider FileSystem -Root \\Server\Share `$\$env:USERNAME -Persist'
+$ST_T = New-ScheduledTaskTrigger -AtLogon
+$ST_S = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -StartWhenAvailable -Hidden
+$ST_P = New-ScheduledTaskPrincipal -GroupId 'Builtin\Users'
+Register-ScheduledTask –TaskName "Map Network Drive" -Action $ST_A –Trigger $ST_T -Settings $ST_S -Principal $ST_P -Force
